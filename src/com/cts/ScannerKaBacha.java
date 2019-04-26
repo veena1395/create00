2fc2d3a6aadc8da7fc393c9cbff4f173082d9881
veena1395 <veena131095@gmail.com>
2019-04-26 09:50:18 +0530

    success

---
+++ src/com/cts/ScannerKaBacha.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import java.util.Scanner;
+
+public class ScannerKaBacha {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+   String empName = s.nextLine();
+   System.out.println("entered name is : "+empName);
+	}
+}
---
+++ src/com/cts/ScannerTest.java
@@ -0,0 +1,12 @@
+package com.cts;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+	public static void main(String[] args) {
+		Scanner m= new Scanner(System.in);
+		String empName=m.nextLine();
+System.out.println("HIS NAME IS:  " +empName);
+
+	}
+}
